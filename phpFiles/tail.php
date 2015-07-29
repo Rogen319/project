89ea5bbf74708275fb0eb53ccc349f25dcba0a91
117n-n711 <hemanta6@hotmail.com>
2015-07-29 12:29:49 +0800

    PHP Changes
    这是我建了一个php的文件，这样我觉得会比较方便。你们要下载xampp或者wampp软件很容易用，我下次会教你们怎么用。。。

---
+++ phpFiles/tail.php
@@ -0,0 +1,23 @@
+<?php 
+	if(!$base){
+	
+	}
+
+?>
+</div>
+		</div>
+		<div id="footw">
+			<div id="foot">
+				&nbsp;
+				<a href="http://e.weibo.com/trendiano" target="_blank">Weibo&nbsp;&nbsp;</a><!-- 这个还没有做完！-->
+				|&nbsp;&nbsp;
+				<a href="http://hailin.tmall.com/" target="_blank">Shop Online &nbsp;&nbsp;</a><!-- 这个还没有做完！-->
+				|&nbsp;&nbsp;
+				<a href="/contact/index.html" target="_blank">Contact Us&nbsp;&nbsp;</a><!-- 这个还没有做完！-->
+				|&nbsp;&nbsp;
+				<a href="http://www.trendy-global.com/" target="_blank">HaiLin Group Official Website &nbsp;&nbsp;</a><!-- 这个还没有做完！-->
+				|&nbsp;&nbsp;
+			</div>
+		</div>
+	</body>
+</html>
